72854e4b1329c4186e6acbefbbee3c1a11d2f99c
topserve <71856516+topserve@users.noreply.github.com>
2022-04-23 17:01:58 -0300

    inicio de app

---
+++ banco/dfsdfsdf/src/pages/Signin/index.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Signin(){
+    return(
+        <View>
+            <Text>Ola Bem vindo</Text>
+        </View>
+    );
+}
